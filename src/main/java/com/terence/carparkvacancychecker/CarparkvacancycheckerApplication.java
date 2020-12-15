3f79b25850f1a9b4d704580a6e1df2ff649e2989
Terence Lim Say Jian <terence.lim@go-jek.com>
2020-12-15 14:38:38 +0800

    Initial Commit

---
+++ src/main/java/com/terence/carparkvacancychecker/CarparkvacancycheckerApplication.java
@@ -0,0 +1,13 @@
+package com.terence.carparkvacancychecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarparkvacancycheckerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CarparkvacancycheckerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/terence/carparkvacancychecker/CarparkvacancycheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terence.carparkvacancychecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarparkvacancycheckerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
