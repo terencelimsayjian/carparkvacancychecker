159973fcc8a7078c6ec6f7cc46b8685638dfc062
Terence Lim Say Jian <terence.lim@go-jek.com>
2020-12-16 10:46:06 +0800

    Add integration test for actuator

--- src/test/java/com/terence/carparkvacancychecker/CarparkvacancycheckerApplicationTests.java
+++ src/test/java/com/terence/carparkvacancychecker/CarparkvacancycheckerApplicationTests.java
@@ -1,13 +1,32 @@
 package com.terence.carparkvacancychecker;
 
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
-class CarparkvacancycheckerApplicationTests {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CarparkvacancycheckerApplicationIntegrationTest {
+
+  @Autowired
+  private TestRestTemplate testRestTemplate;
 
   @Test
   void contextLoads() {
   }
 
+  @Test
+  void shouldHaveActuatorHealthEndpoint() throws Exception {
+    ResponseEntity<String> responseEntity = testRestTemplate.getForEntity("/actuator/health", String.class);
+    String body = responseEntity.getBody();
+
+    assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    JSONAssert.assertEquals("{status: UP}", body, JSONCompareMode.STRICT);
+  }
 }
